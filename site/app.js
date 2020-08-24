17e5d45ae88ac9c4028d5df9045b452e8becb6db
srinivoleti1 <70172065+srinivoleti1@users.noreply.github.com>
2020-08-24 13:17:50 -0700

    Update app.js

--- site/app.js
+++ site/app.js
@@ -52,8 +52,8 @@ app.get('/info', function(req, res) {
   if (CURRENTMODE == MODE.TEST) {
     patientdata = {
       "personal": {
-        "name": "Ralph DAlmeida",
-        "age": 38,
+        "name": "Srini Voleti",
+        "age": 48,
         "gender": "male",
         "street": "34 Main Street",
         "city": "Toronto",
